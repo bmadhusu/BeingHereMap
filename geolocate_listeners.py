c9204e7c6ac094918a9bc6e85493a7a8c5536db0
bmadhusu <madhu_lists@yahoo.com>
2018-03-20 21:45:31 -0400

    Add files via upload

--- geolocate_listeners.py
+++ geolocate_listeners.py
@@ -4,7 +4,7 @@ import pandas as pd
 def GoogGeoAPI(address,api="",delay=5):
   base = r"https://maps.googleapis.com/maps/api/geocode/json?"
   addP = "address=" + address.replace(" ","+")
-  GeoUrl = base + addP + "&key=" + api
+  GeoUrl = base + addP #+ "&key=" + api
 
   response = urllib.request.urlopen(GeoUrl)
   jsonRaw = response.read().decode('utf-8')
@@ -22,27 +22,44 @@ def GoogGeoAPI(address,api="",delay=5):
 locations_list = []
 
 xl = pd.ExcelFile(sys.argv[1])
-df1 = xl.parse('OnDemandRegional')
+df_ondemand = xl.parse('OnDemandRegional')
+df_ondemand = df_ondemand[pd.notnull(df_ondemand['Country'])]
 
-# f = open(sys.argv[1], 'r') # opens the csv file
-try:
-    # reader = csv.reader(f)  # creates the reader object
-    # next(reader, None)
-    #for row in reader:   # iterates the rows of the file in orders
-    for index, row in df1.iterrows():
-        if index > 0:
-          print (row)    # prints each row
-        # geoR= GoogGeoAPI(address=row[0])
-          geoR= GoogGeoAPI(address=row[0])
 
-        # locations_list.append({"country":row[0],"num_listeners":row[1],"lat":geoR[1],"lon":geoR[2]})
-          locations_list.append({"country":row[0],"lat":geoR[1],"lon":geoR[2]})
+df_live = xl.parse('LiveRegional')
+df_live = df_live[pd.notnull(df_live['Country'])]
 
-finally:
-    # f.close()      # closing
-    print("BYE!")      # closing
+countries = set()
 
-print(locations_list)
+# Parse the live set of countries and throw them into a set
+for index, row in df_live.iterrows():
+  if (row[0]=="United States"):
+    countries.add(row[1]) # if country is USA, need to add the region which is the state
+  else:
+    countries.add(row[0])
+
+
+for index, row in df_ondemand.iterrows():
+  if index == 0:
+    continue
+  if row[0] != "United States": # we ignore USA because the states will be covered above
+    countries.add(row[0])
+
+
+print(countries)
+
+while bool(countries):
+  print("Num of countries remaining: ", len(countries))
+  c = countries.pop()
+  # print(c)
+  geoR=GoogGeoAPI(address=c)
+  if geoR != [None, None, None]:
+    locations_list.append({"country":c,"lat":geoR[1],"lon":geoR[2]})
+    # countries.discard(c)
+    print(geoR)
+  else:
+    print("google rejected: ", c, " and will try later")
+    countries.add(c)
 
 fname = sys.argv[1]+".js"
 print (fname)
